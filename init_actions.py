70704426be086b653dbd38a4f89e4ac801a492cf
Justin <justin@example.com>
2024-12-26 12:28:11 -0600

    true up

--- init_actions.py
+++ init_actions.py
@@ -1,7 +1,7 @@
 from app import create_app
 from app.utils.init_db import init_actions
-from config_migrate import MigrateConfig
+from config import config
 
-app = create_app(MigrateConfig)
+app = create_app(config['production'])
 with app.app_context():
     init_actions()
